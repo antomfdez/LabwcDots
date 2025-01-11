71a6d24393d5c112eb749d8d54bdf3a8ef1510aa
antomfdez <antonio.mesa.fdez@gmail.com>
2025-01-11 10:21:06 +0100

    Added directory scripts to ~/scripts with copy-dots.sh and wall-set.sh

---
+++ scripts/copy-dots.sh
@@ -0,0 +1,6 @@
+cp -r ~/.config/labwc .
+cp -r ~/.config/alacritty .
+cp -r ~/.config/sfwbar .
+cp -r ~/.themes/Nightmare-01 .
+cp -r ~/.config/kanshi .
+cp -r ~/scripts .
---
+++ scripts/wall-set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ensure an argument (wallpaper path) is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 /path/to/wallpaper"
+    exit 1
+fi
+
+WALLPAPER_PATH="$1"
+
+# Check if the file exists
+if [ ! -f "$WALLPAPER_PATH" ]; then
+    echo "Error: File not found at $WALLPAPER_PATH"
+    exit 1
+fi
+
+# Copy the wallpaper to ~/.labwc
+LABWC_WALLPAPER_FILE="$HOME/.labwc"
+cp "$WALLPAPER_PATH" "$LABWC_WALLPAPER_FILE"
+
+# Set ~/.labwc as the wallpaper using swaybg
+swaybg -i "$LABWC_WALLPAPER_FILE" -m fill >/dev/null 2>&1 & disown
+
+echo "Wallpaper $WALLPAPER_PATH set :)"
